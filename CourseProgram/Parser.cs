39f9e70037273abf6e123fb3229916c4a600da44
Igor Efremov <efremoff-igor@mail.ru>
2017-12-10 17:21:24 +0500

    Migrated to NetCore Can get week info from html

---
+++ CourseProgram/Parser.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using HtmlAgilityPack;
+
+namespace CourseProgram
+{
+    public class Parser
+    {
+        HtmlDocument document = new HtmlDocument();
+        public Parser(StreamReader stream)
+        {
+            document.Load(stream);
+        }
+
+        public List<string> GetWeeks()
+        {
+            var weeks = document.DocumentNode.SelectNodes(@".//*[@class = 'week']");
+            foreach (var node in weeks)
+            {
+                
+                Console.WriteLine(node.InnerText.Split("expand")[0]);
+            }
+
+            throw new NotImplementedException();
+        }
+    }
+}
