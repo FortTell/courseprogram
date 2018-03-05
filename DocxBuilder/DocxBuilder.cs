e3aea965c3765e811b5788fcb40393c5a4f89e2a
Igor Efremov <efremoff-igor@mail.ru>
2018-03-05 14:08:16 +0500

    Discipline template now in separate file (1 disc/module as of yet!)

--- DocxBuilder/DocxBuilder.cs
+++ DocxBuilder/DocxBuilder.cs
@@ -27,12 +27,47 @@ namespace DocxBuilder
 
                 var teachers = pi.teachers;
                 var teacherT = d.InsertTable(2, 6);
-                foreach (TableBorderType borderType in Enum.GetValues(typeof(TableBorderType)))
-                    teacherT.SetBorder(borderType,
-                        new Border(BorderStyle.Tcbs_single, BorderSize.one, 1, Color.Black));
+                SetTableBorder(teacherT);
 
                 d.Save();
             }
+        }
+
+        private static void SetTableBorder(Table t)
+        {
+            foreach (TableBorderType borderType in Enum.GetValues(typeof(TableBorderType)))
+                t.SetBorder(borderType,
+                    new Border(BorderStyle.Tcbs_single, BorderSize.one, 1, Color.Black));
+        }
+
+        private static void FillEducResults(DocX template)
+        {
+            var rnd = new Random();
+            var educResTable = template.Tables[3];
+            using (var d = DocX.Load("competence.docx"))
+            {
+                var t = d.Tables[0];
+                var rowCount = t.RowCount;
+                for (int i = 0; i < (rowCount / 2) + 1; i++)
+                {
+                    educResTable.InsertRow();
+                    var rowIndex = rnd.Next(0, t.RowCount);
+                    var row = t.Rows[rowIndex];
+                    educResTable.Rows[educResTable.RowCount - 1].Cells[1].Paragraphs[0]
+                        .Append(row.Cells[0].Paragraphs[0].Text);
+                    var parasCount = row.Cells[1].Paragraphs.Count;
+                    var cell = educResTable.Rows[educResTable.RowCount - 1].Cells[2];
+                    cell.RemoveParagraphAt(0);
+                    for (int j = 0; j < (parasCount / 2) + 1; j++)
+                    {
+                        var para = row.Cells[1].Paragraphs[rnd.Next(0, row.Cells[1].Paragraphs.Count)];
+                        cell.InsertParagraph(para);
+                        row.Cells[1].RemoveParagraph(para);
+                    }
+                    t.RemoveRow(rowIndex);
+                }
+            }
+            SetTableBorder(educResTable);
         }
 
         public static void BuildDocxFromTemplate(ParseInfo pi, string templateFilename)
@@ -40,9 +75,12 @@ namespace DocxBuilder
             if (!Directory.Exists("out"))
                 Directory.CreateDirectory("out");
             LoadReplacements(pi);
+                
 
             using (var d = DocX.Load(templateFilename))
             {
+                d.InsertDocument(DocX.Load("disc_template.docx"));
+                FillEducResults(d);
                 var paragraphs = d.Paragraphs.Where(p => p.Text != "");
                 foreach (var p in paragraphs)
                     foreach (var repl in TemplateReplacements)
@@ -57,11 +95,14 @@ namespace DocxBuilder
             TemplateReplacements = new Dictionary<string, string>
             {
                 { "<COURSE_NAME>", pi.courseName },
+                { "<DISC_NAME>", pi.courseName }, //no support for multi-discipline modules yet
                 { "<YEAR>", DateTime.Now.Year.ToString()},
                 { "<CITY>", "Екатеринбург" },
                 { "<NAME>", "Иванов И.И." },
                 { "<POSITION>", "Искатель интересных историй" },
-                { "<UNIVERSITY_NAME>", "ВУЗ им. Иванова И.И." }
+                { "<UNIVERSITY_NAME>", "ВУЗ им. Иванова И.И." },
+                { "<SEMESTER_NO>", "5"},
+                { "<TEST_TYPE>", "экзамен"}
             };
         }
     }
